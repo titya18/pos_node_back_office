d2fb070f8abc83cba5dc944a31790e2bca89c204
titya18 <tityalorn@gmail.com>
2025-04-11 22:07:12 +0700

    Map permissions to API second time

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -13,7 +13,7 @@ import {
 // Rate limiter configuration
 const signInLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // limit each IP to 5 requests per windowMs.
+    max: 5, // limit each IP to 5 requests per windowMs
     message: { error: "Too many login attempts from this IP, please try again later" }, // Ensure response is JSON
     statusCode: 429 // Too Many Requests
 });
